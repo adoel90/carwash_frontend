d44d258f6f4951be16fb5d00e8f1d022776a134c
davidkurniakristiadi <davidkurniakristiadi@gmail.com>
2017-10-10 19:52:26 +0700

    Added Home view component.

---
+++ src/actions/HomeView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HomeView extends React.Component {
+	render() {
+		const {
+			props,
+			state
+		} = this;
+
+		return (
+			<div id="home">
+			</div>
+		)
+	}
+}
+
+export default HomeView;
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Home from '../components/Home';
 
 class HomeContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			counter: 0,
+			value: 'Hey click me plox?',
+		}
+	}
+
+	handleClick() {
+		this.setState({
+			counter: this.state.counter + 1
+		})
+	}
+
+
 	render() {
-		return <h1 className="flex">TES</h1>
+		console.log(this.state);
+
+		return <Home {...this.state} handleClick={this.handleClick} />
 	}
 }
 

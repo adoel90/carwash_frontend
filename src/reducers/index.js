c0fa08654e83ab655a822f4f9de612d46e09e610
Andre Rizkiana Wandira <andre.rizkiana.w@gmail.com>
2017-10-23 11:28:54 +0700

    Temp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import User from './user.reducer';
 import member from './member.reducer';
 
 const rootReducer = combineReducers({
 	User,
-	member
+	member,
+	form: formReducer
 });
 
 export default rootReducer;

080e92f84980862d669b46c61636c9f816bdc968
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-01-29 14:42:22 +0700

    Removed old API_PATH.

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ import Cookies from 'universal-cookie';
 
 const constant = {
 	CLIENT_ROOT_URL: 'localhost:3000',
-	// API_PATH: 'http://128.199.234.141:3302/',		//production
-	API_PATH: 'http://128.199.234.141:3307/'		//development
+	API_PATH: 'http://128.199.234.141:3307/'		//production
 }
 
 export {

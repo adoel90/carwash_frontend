30181562ea514aa570f9de3e0724b872e05fae99
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-01-29 15:35:51 +0700

    Changed to development

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import Cookies from 'universal-cookie';
 
 const constant = {
 	CLIENT_ROOT_URL: 'localhost:3000',
-	// API_PATH: 'http://128.199.234.141:3302/'	// development test
-	API_PATH: 'http://128.199.234.141:3307/'		//production
+	API_PATH: 'http://128.199.234.141:3302/'	// development test
+	// API_PATH: 'http://128.199.234.141:3307/'		//production
 }
 
 export {

ff51109edf2472e6d33edc8b8897c2327ed715c7
Andre Rizkiana Wandira <andre.rizkiana.w@gmail.com>
2018-02-27 10:31:32 +0700

    Add Feature Create Admin Vendor in page Admin - PART02

---
+++ src/views/Admin/AdminVendor/AdminVendorCreateView.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Panel } from '../../../components/Panel';
+import { Form, FormField } from '../../../layouts/Form';
+import { Input, InputGroup, Select } from '../../../components/Input';
+import { Button } from '../../../components/Button';
+
+const AdminVendorCreateView = props => { 
+    
+    const {
+
+        handleFormSubmit,
+        handleInputChange,
+        newAdminVendorState
+    } = props;
+
+    return(
+
+        <div className="admin-user-create">
+            <Panel>
+                <div className="admin-user__heading padding-bottom-large">
+                    <h4 className="heading-title">Form Admin Vendor Baru</h4>
+                    <h6 className="heading-subtitle">Tempor nostrud cupidatat officia sit ullamco eu pariatur ullamco quis laborum nulla ipsum.</h6>
+                </div>
+
+                <Form onSubmit={handleFormSubmit}>
+                    <FormField label="Nama Vendor">
+                        <Input name="cafe_name" type="text" placeholder="Masukkan nama vendor baru" onChange={(e) => handleInputChange('newAdminVendorState', e) } />
+                    </FormField>
+
+                    <FormField label="Employee Full Name">
+                        <Input name="fullname" type="text" placeholder="Masukkan Full Name Employee " onChange={(e) => handleInputChange('newAdminVendorState', e) } />
+                    </FormField>
+
+                   <FormField label="Alamat Email">
+                        <Input name="email" type="text" placeholder="Masukkan alamat email user (jika ada)" onChange={(e) => handleInputChange('newAdminVendorState', e) } />
+                    </FormField>
+
+                    <FormField label="Username ">
+                        <Input name="username" type="text" placeholder="Masukkan nickname employee" onChange={(e) => handleInputChange('newAdminVendorState', e) } />
+                    </FormField>
+
+                    <FormField label="Password">
+                        <InputGroup>
+                            <Input name="password" type="password" placeholder="Masukkan kata sandi" onChange={(e) => handleInputChange('newAdminVendorState', e) } />
+                            <Input name="confirmPassword" type="password" placeholder="Ulangi kata sandi" onChange={(e) => handleInputChange('newAdminVendorState', e) } />                            
+                        </InputGroup>
+                    </FormField>
+
+                    <Button type="submit">Simpan</Button>
+
+
+                </Form>
+            </Panel>
+
+        </div>
+    )
+}
+
+AdminVendorCreateView.propTypes = {
+    
+};
+
+export default AdminVendorCreateView;
